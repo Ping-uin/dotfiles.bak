c76be12aa494fd44c6e2849dce0b431eb411a47b
Maximilian Sunkovsky <maximilian.sunkovsky@bright-testing.de>
2024-09-19 16:39:13 +0200

    Initial commit: Add .gitignore and nvim configuration files

---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,107 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts =  {
+      ensure_installed = {
+        "black",
+        "rust-analyzer",
+        "pyright",
+        "mypy",
+        "ruff",
+        "debugpy",
+      },
+    },
+  },
+  {
+    "nvim-neotest/nvim-nio",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function ()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function (_, opts)
+      require('rust-tools').setup(opts)
+    end
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function (_, opts)
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function (_, opts)
+      local path = "C:/Users/maximilian.sunkovsky/AppData/Local/nvim-data/mason/packages/debugpy/venv/bin/python"
+      require("dap-python").setup(path)
+      require("core.utils").load_mappings("dap_python")
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    --- event = "VeryLazy",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require "dap"
+      local dapui = require "dapui"
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+  {
+    'saecki/crates.nvim',
+    ft = {"rust", "toml"},
+    config = function (_, opts) 
+      local crates = require('crates')
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function ()
+      local M = require "plugins.configs.cmp"
+      table.insert(M.sources, {name = "crates"})
+      return M
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+  ---  event = "VeryLazy",
+    ft = {"python"},
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  }
+}
+return plugins
